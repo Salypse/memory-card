465f6896424cb2a5ce871065d947c70c80558237
Salypse <84153483+Salypse@users.noreply.github.com>
2026-02-27 11:34:21 -0600

    Add styling to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import "../styles/Header.css";
+
 export function Header(props) {
    const { score, highScore } = props;
 
    return (
-      <header>
+      <header className="header">
          <h1>Memory Card</h1>
          <div className="scores">
             <p>Score: {score}</p>
